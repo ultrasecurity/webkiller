c78fdc23b1858a6c90cdab8ee1b3e9d9af5eaac9
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-12 03:54:49 +0430

    Update webkiller.py

--- webkiller.py
+++ webkiller.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3  
 import sys
-import socket
 import os
 import time
 from helplist import helpp
@@ -15,7 +14,10 @@ except:
     print(Fore.RED+"""\n Please Install colorama\n
     pip3 install colorama
         """)
-
+try:
+    import socket
+except ImportError:
+    os.system("pip install socket")
 #---------------------------
 
 try:
